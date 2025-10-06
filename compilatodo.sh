2da345202b8fb5c061c125f509229d222117f753
fspivak <fespivak@itba.edu.ar>
2025-10-06 06:46:57 -0300

    Add Dockerfile and improve build script flexibility
    Introduces a Dockerfile for a Debian-based build environment with necessary tools. Updates compilatodo.sh to accept an optional parameter for selecting the build target, allowing compilation with either MM Simple or MM Buddy.

--- compilatodo.sh
+++ compilatodo.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# Uso: ./compilatodo.sh         # Compila con MM Simple
+#      ./compilatodo.sh buddy   # Compila con MM Buddy
+
+TARGET=${1:-""}  # Parametro opcional: vacio (simple) o "buddy"
+
 docker start SO-TPE2
 docker exec -it SO-TPE2 make clean -C /root/Toolchain
 docker exec -it SO-TPE2 make clean -C /root
 docker exec -it SO-TPE2 make -C /root/Toolchain
-docker exec -it SO-TPE2 make -C /root
+docker exec -it SO-TPE2 make $TARGET -C /root
 docker exec -it SO-TPE2 chmod 777 /root/Image/x64BareBonesImage.qcow2
 docker stop SO-TPE2
