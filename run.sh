4aaf47cabc5820923fe20228e829cee76a8d8af8
fspivak <fespivak@itba.edu.ar>
2025-10-15 12:28:26 -0300

    chore: update build configuration and scripts
    - Update Makefile to include commands/ directory
    - Disable QEMU sound in run.sh for better development experience
    - Ensure proper compilation of new command architecture
    
    Co-Authored-By: Jeronimo Lopez Vila <152904297+Jeronimolv@users.noreply.github.com>
    Co-Authored-By: PabloOGermano <119269874+PabloOGermano@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ if [[ $1 == "debug" || $1 == "gdb" ]]; then
     echo ""
     
     # Start QEMU with debug server on port 1234
-    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
 elif [[ $1 == "debug-int" ]]; then
     echo -e "${BLUE}Starting QEMU in debug mode with interrupt logging...${NC}"
     echo -e "${YELLOW}QEMU will log all interrupts/exceptions${NC}"
@@ -34,11 +34,11 @@ elif [[ $1 == "debug-int" ]]; then
     echo ""
     
     # Start QEMU with debug server and interrupt logging
-    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker -d int 2>&1 | grep "v=" &
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int 2>&1 | grep "v=" &
     echo -e "${BLUE}QEMU started in background with interrupt logging${NC}"
     echo -e "${YELLOW}Interrupts are being logged to stderr${NC}"
 else
     echo -e "${GREEN}Starting QEMU in normal mode...${NC}"
     echo -e "${RED}Press Ctrl+Alt+Q to kill QEMU${NC}"
-    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 fi
