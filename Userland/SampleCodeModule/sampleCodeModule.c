da8ed66a492c06098e50258f764f2e3864d6467c
fspivak <fespivak@itba.edu.ar>
2025-11-03 14:24:45 -0300

    feat: shell as process

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,8 +1,17 @@
 /* sampleCodeModule.c */
+#include "include/libasmUser.h"
+#include "include/stinUser.h"
 #include "include/terminal.h"
+#include <stddef.h>
 
 /* Punto de entrada de userland */
 int main() {
-	terminal();
+	char *argv[] = {NULL};
+	int pid = create_process("shell", (void *) terminal, 0, argv, 1);
+	if (pid < 0) {
+		print("Error: could not create clock process\n");
+		return 1;
+	}
+	waitpid(pid);
 	return 0;
 }
