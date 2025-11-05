cf9f87609af473a6b1c0d020d3bf648efb48698d
fspivak <fespivak@itba.edu.ar>
2025-11-05 15:28:33 +0000

    refactor: use validation functions in process management commands

--- Userland/SampleCodeModule/commands/kill_cmd.c
+++ Userland/SampleCodeModule/commands/kill_cmd.c
@@ -2,35 +2,11 @@
 #include "../include/libasmUser.h"
 #include "../include/stinUser.h"
 #include "../include/stringUser.h"
-#include "../tests/include/test_util.h"
 #include <stddef.h>
 
 static void kill_main(int argc, char **argv) {
-	if (argc < 2 || argv == NULL || argv[1] == NULL) {
-		print("Usage: kill <pid>\n");
-		return;
-	}
-
-	int pid = satoi(argv[1]);
+	int pid = validate_pid_arg("kill", argc, argv, 1);
 	if (pid <= 0) {
-		print("ERROR: Invalid PID\n");
-		return;
-	}
-
-	ProcessInfo processes[64];
-	int count = ps(processes, 64);
-	int found = 0;
-	for (int i = 0; i < count; i++) {
-		if (processes[i].pid == pid) {
-			found = 1;
-			break;
-		}
-	}
-
-	if (!found) {
-		print("kill: ");
-		printBase(pid, 10);
-		print(": arguments must be process or job IDs\n");
 		return;
 	}
 
@@ -43,8 +19,7 @@ static void kill_main(int argc, char **argv) {
 
 void kill_cmd(int argc, char **argv) {
 	int pid_kill = create_process("kill", kill_main, argc, argv, 0);
-	if (pid_kill < 0) {
-		print("ERROR: Failed to create process kill\n");
+	if (!validate_create_process_error("kill", pid_kill)) {
 		return;
 	}
 
--- Userland/SampleCodeModule/commands/nice_cmd.c
+++ Userland/SampleCodeModule/commands/nice_cmd.c
@@ -2,43 +2,16 @@
 #include "../include/libasmUser.h"
 #include "../include/stinUser.h"
 #include "../include/stringUser.h"
-#include "../tests/include/test_util.h"
 #include <stddef.h>
 
 static void nice_main(int argc, char **argv) {
-	if (argc < 3 || argv == NULL || argv[1] == NULL || argv[2] == NULL) {
-		print("Usage: nice <pid> <priority>\n");
-		print("Priority range: 0-255 (default: 4)\n");
-		return;
-	}
-
-	int pid = satoi(argv[1]);
-	int priority = satoi(argv[2]);
-
+	int pid = validate_pid_arg("nice", argc, argv, 1);
 	if (pid <= 0) {
-		print("ERROR: Invalid PID\n");
 		return;
 	}
 
-	if (priority < 0 || priority > 255) {
-		print("ERROR: Priority must be between 0 and 255\n");
-		return;
-	}
-
-	ProcessInfo processes[64];
-	int count = ps(processes, 64);
-	int found = 0;
-	for (int i = 0; i < count; i++) {
-		if (processes[i].pid == pid) {
-			found = 1;
-			break;
-		}
-	}
-
-	if (!found) {
-		print("nice: ");
-		printBase(pid, 10);
-		print(": arguments must be process or job IDs\n");
+	int priority = validate_priority_arg("nice", argc, argv, 2);
+	if (priority < 0) {
 		return;
 	}
 
@@ -59,8 +32,7 @@ static void nice_main(int argc, char **argv) {
 
 void nice_cmd(int argc, char **argv) {
 	int pid_nice = create_process("nice", nice_main, argc, argv, 1);
-	if (pid_nice < 0) {
-		print("ERROR: Failed to create process nice\n");
+	if (!validate_create_process_error("nice", pid_nice)) {
 		return;
 	}
 
