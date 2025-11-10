bdbefcb59badb92dd103e5970046c26f8ff462ca
fspivak <fespivak@itba.edu.ar>
2025-11-10 00:56:10 +0000

    refactor: update stinUser interface docs

--- Userland/SampleCodeModule/include/stinUser.h
+++ Userland/SampleCodeModule/include/stinUser.h
@@ -4,15 +4,11 @@ char getchar();
 
 void putchar(char carac);
 
-void printHexa(uint64_t value);
-
-void print(char *string);
+void print_format(const char *format, ...);
 
 void printColor(char *string, int color, int bg);
 
 void putCharColor(char carac, int color, int bg);
-
-void printBase(uint64_t value, uint32_t base);
 
 void zoomIn();
 
@@ -37,5 +33,3 @@ uint64_t getMS();
 int numeroAleatorioEntre(int min, int max, uint64_t *seed);
 
 unsigned int generarNumeroAleatorio(uint64_t *seed);
-
-uint64_t getMS();
